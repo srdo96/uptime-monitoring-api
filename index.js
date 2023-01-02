ef6c30c8244878379ebbaafcb4137ba8ae6b74d2
Sakib Rahman <seu.sakib@gmail.com>
2023-01-02 16:38:38 +0600

    create server

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // dependencies
 const http = require('node:http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listing to port ${app.config.port}`);
+    });
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+    // response handle
+    res.end('Hello from server');
+};
+
+// start the server
+app.createServer();
