6045ec8a78214b836c0a945f8c49c4e22b1ba198
Sakib Rahman <seu.sakib@gmail.com>
2023-01-02 20:18:59 +0600

    refactor handleReqRes

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,33 @@
+// dependence
+const url = require('node:url');
+const { StringDecoder } = require('node:string_decoder');
+
+// module scaffolding
+const handler = {};
+
+handler.handleReqRes = (req, res) => {
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+
+        // response handling
+        res.end(realData);
+    });
+};
+
+module.exports = handler;
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // dependencies
 const http = require('node:http');
-const url = require('node:url');
-const { StringDecoder } = require('node:string_decoder');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
 // app object - module scaffolding
 const app = {};
@@ -20,30 +19,7 @@ app.createServer = () => {
 };
 
 // handle Request Response
-app.handleReqRes = (req, res) => {
-    // request handling
-    // get the url and parse it
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
-    });
-
-    req.on('end', () => {
-        realData += decoder.end();
-
-        // response handling
-        res.end(realData);
-    });
-};
+app.handleReqRes = handleReqRes;
 
 // start the server
 app.createServer();
